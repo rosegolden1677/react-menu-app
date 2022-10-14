133e1021d07a964f6f041e6b9c635611f192e84e
rosegolden1677 <makela.adams@yahoo.com>
2022-10-14 16:33:59 -0700

    add routing & icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Menu from './pages/Menu';
+import Reviews from './pages/Reviews';
+import About from './pages/About';
 
 function App() {
   return (
-    <div>
-      hello
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" index element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 

ddadc1167708ef2a7ded35c42cef9e3e65e9a053
rosegolden1677 <makela.adams@yahoo.com>
2022-10-14 16:38:17 -0700

    add data file

---
+++ src/data.js
@@ -0,0 +1,214 @@
+const breakfast = [
+  {
+    id: 1,
+    name: 'orange juice',
+    category: 'beverages',
+    price: 3.99,
+    desc: 'Salutatus eloquentiam mea ad, qui error lucilius eu.'
+  },
+  {
+    id: 2,
+    name: 'apple juice',
+    category: 'beverages',
+    price: 3.99,
+    desc: 'Salutatus eloquentiam mea ad, qui error lucilius eu.'
+  },
+  {
+    id: 3,
+    name: 'cranberry juice',
+    category: 'beverages',
+    price: 3.99,
+    desc: 'Salutatus eloquentiam mea ad, qui error lucilius eu.'
+  },
+  {
+    id: 4,
+    name: 'cafe latte',
+    category: 'beverages',
+    price: 7.99,
+    desc: 'Salutatus eloquentiam mea ad, qui error lucilius eu.'
+  },
+  {
+    id: 5,
+    name: 'black coffee',
+    category: 'beverages',
+    price: 5.99,
+    desc: 'Salutatus eloquentiam mea ad, qui error lucilius eu.'
+  },
+  {
+    id: 6,
+    name: 'vanilla cappuccino',
+    category: 'beverages',
+    price: 7.99,
+    desc: 'Salutatus eloquentiam mea ad, qui error lucilius eu.'
+  },
+  {
+    id: 7,
+    name: 'matcha latte',
+    category: 'beverages',
+    price: 5.99,
+    desc: 'Salutatus eloquentiam mea ad, qui error lucilius eu.'
+  },
+  {
+    id: 8,
+    name: 'chai tea',
+    category: 'beverages',
+    price: 7.99,
+    desc: 'Salutatus eloquentiam mea ad, qui error lucilius eu.'
+  },
+  {
+    id: 9,
+    name: 'green tea',
+    category: 'beverages',
+    price: 4.99,
+    desc: 'Salutatus eloquentiam mea ad, qui error lucilius eu.'
+  },
+  {
+    id: 10,
+    name: 'mint tea',
+    category: 'beverages',
+    price: 4.99,
+    desc: 'Salutatus eloquentiam mea ad, qui error lucilius eu.'
+  },
+  {
+    id: 11,
+    name: 'cinnamon rolls',
+    category: 'pastries',
+    price: 8.99,
+    desc: 'Salutatus eloquentiam mea ad, qui error lucilius eu.'
+  },
+  {
+    id: 12,
+    name: 'cheese danish',
+    category: 'pastries',
+    price: 8.99,
+    desc: 'Salutatus eloquentiam mea ad, qui error lucilius eu.'
+  },
+  {
+    id: 13,
+    name: 'scones',
+    category: 'pastries',
+    price: 8.99,
+    desc: 'Salutatus eloquentiam mea ad, qui error lucilius eu.'
+  },
+  {
+    id: 14,
+    name: 'muffins',
+    category: 'pastries',
+    price: 8.99,
+    desc: 'Salutatus eloquentiam mea ad, qui error lucilius eu.'
+  },
+  {
+    id: 15,
+    name: 'banana bread',
+    category: 'pastries',
+    price: 8.99,
+    desc: 'Salutatus eloquentiam mea ad, qui error lucilius eu.'
+  },
+  {
+    id: 16,
+    name: 'biscuits',
+    category: 'pastries',
+    price: 8.99,
+    desc: 'Salutatus eloquentiam mea ad, qui error lucilius eu.'
+  },
+  {
+    id: 17,
+    name: 'croissant',
+    category: 'pastries',
+    price: 8.99,
+    desc: 'Salutatus eloquentiam mea ad, qui error lucilius eu.'
+  },
+  {
+    id: 18,
+    name: 'croissant sandwich',
+    category: 'breakfast',
+    price: 15.99,
+    desc: 'Salutatus eloquentiam mea ad, qui error lucilius eu.'
+  },
+  {
+    id: 19,
+    name: 'smoked salmon bagel',
+    category: 'breakfast',
+    price: 15.99,
+    desc: 'Salutatus eloquentiam mea ad, qui error lucilius eu.'
+  },
+  {
+    id: 20,
+    name: 'ultimate bagel sandwich',
+    category: 'breakfast',
+    price: 15.99,
+    desc: 'Salutatus eloquentiam mea ad, qui error lucilius eu.'
+  },
+  {
+    id: 21,
+    name: 'huevos rancheros',
+    category: 'breakfast',
+    price: 15.99,
+    desc: 'Salutatus eloquentiam mea ad, qui error lucilius eu.'
+  },
+  {
+    id: 22,
+    name: 'chilaquiles',
+    category: 'breakfast',
+    price: 15.99,
+    desc: 'Salutatus eloquentiam mea ad, qui error lucilius eu.'
+  },
+  {
+    id: 23,
+    name: 'loaded burrito',
+    category: 'breakfast',
+    price: 15.99,
+    desc: 'Salutatus eloquentiam mea ad, qui error lucilius eu.'
+  },
+  {
+    id: 24,
+    name: 'french toast',
+    category: 'breakfast',
+    price: 15.99,
+    desc: 'Salutatus eloquentiam mea ad, qui error lucilius eu.'
+  },
+  {
+    id: 25,
+    name: 'fluffy pancakes',
+    category: 'breakfast',
+    price: 15.99,
+    desc: 'Salutatus eloquentiam mea ad, qui error lucilius eu.'
+  },
+  {
+    id: 26,
+    name: 'waffles',
+    category: 'breakfast',
+    price: 15.99,
+    desc: 'Salutatus eloquentiam mea ad, qui error lucilius eu.'
+  },
+  {
+    id: 27,
+    name: 'grits',
+    category: 'sides',
+    price: 12.99,
+    desc: 'Salutatus eloquentiam mea ad, qui error lucilius eu.'
+  },
+  {
+    id: 28,
+    name: 'breakfast potatoes',
+    category: 'sides',
+    price: 12.99,
+    desc: 'Salutatus eloquentiam mea ad, qui error lucilius eu.'
+  },
+  {
+    id: 29,
+    name: 'oatmeal',
+    category: 'sides',
+    price: 12.99,
+    desc: 'Salutatus eloquentiam mea ad, qui error lucilius eu.'
+  },
+  {
+    id: 30,
+    name: 'fruit salad',
+    category: 'sides',
+    price: 12.99,
+    desc: 'Salutatus eloquentiam mea ad, qui error lucilius eu.'
+  }
+];
+
+export default breakfast;
